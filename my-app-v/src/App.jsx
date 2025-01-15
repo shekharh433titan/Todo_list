96865c175e21d1bf790bb7820eb859a1c7874094
HS <hs@BMDVHIMANSHUS.local>
2025-01-15 12:23:35 +0530

    adding pages and router in app

--- my-app-v/src/App.jsx
+++ my-app-v/src/App.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
-import { QueryClientProvider } from "@tanstack/react-query";
-import MyQueryClient from "./logic/MyQueryClient"
+import React, { useState, StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Todo from "./components/todo";
 
-import { TodoProvider } from "./logic/useTodoLogic";
+import TodoPage from "./pages/TodoPage"
+import TestPage from "./pages/TestPage"
 
-// Root Component to wrap App with QueryClientProvider
-// It acts as a context provider, making the QueryClient instance available to 
-// all components within its child hierarchy.
-
-// If you want to initialize this hook once for reuse across components, 
-// consider using a React Context to share the logic and state globally 
-// while ensuring proper React Hook rules are followed.
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TodoPage />
+  },
+  {
+    path: "/testpage",
+    element: <TestPage />
+  }
+]);
 
 function App() {
-  const queryClient = MyQueryClient();
   return (
-    <QueryClientProvider client={queryClient}>
-      <TodoProvider> <Todo></Todo></TodoProvider>
-    </QueryClientProvider>
+    <StrictMode>
+      <RouterProvider
+        router={router}
+        future={{
+          v7_startTransition: true,
+        }} />
+    </StrictMode>
   );
 }
-
-
 
 export default App;
